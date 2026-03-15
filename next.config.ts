4807713667664706f097a4c35751f0ab8813adaa
yonghuitang` <yonghuitang123@gmail.com>
2026-03-15 13:51:40 -0700

    fix: remove invalid next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    browsersListForSwc: true,
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'placehold.co' },
